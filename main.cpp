69d7908d568cf65d40463f3bef498fef3cf52c35
TeoPigna19 <91795015+TeoPigna19@users.noreply.github.com>
2021-11-14 16:10:24 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,31 @@
 #include <iostream>
 using namespace std;
 
-int main() {
-   return 0;
+int main(){
+
+    int n, i=1, a=0, b=1, c;
+    cin>>n;
+
+    if(n>=2)
+    {
+
+        cout<<b<<endl;
+        for(i=2; i<=n; i++)
+        {
+
+            c=a+b;
+            cout<<c<<endl;
+            a=b;
+            b=c;
+
+        }
+
+    }
+    else{
+        cout<<"errore"<<endl;
+
+    }
+
+
+    return 0;
 }
